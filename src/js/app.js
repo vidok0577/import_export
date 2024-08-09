0760eda6add13e1cf36b2f1577b2eec1a0c2375e
Igor Vazhenin <vidok0577@gmail.com>
2024-08-09 11:38:17 +0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import '../css/style.css';
+import Game, {
+    GameSavingData,
+    readGameSaving as loadGame,
+    writeGameSaving as saveGame
+} from './game.js';
+
+const game = new Game();
+
+game.start();
